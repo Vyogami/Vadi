69dae55381cbcb339c101d5af202ee4445fb1b11
Shivam Shandilya <shiv.05cs@gmail.com>
2024-04-08 03:22:37 +0530

    chore(parser): add parser for expression(+,-)... - add term(*,/) - add factor(int, float)

---
+++ src/vadi/parser.py
@@ -0,0 +1,52 @@
+from typing import List
+
+from tokens import Token
+
+
+class Parser:
+    def __init__(self, tokens: List[Token]) -> None:
+        self.tokens: List[Token] = tokens  # type: ignore  # noqa: PGH003 # To disable pylint and ruff warnings
+        self.idx: int = 0
+
+        # if tokens is not None:
+        self.token: Token = self.tokens[self.idx]
+
+    def factor(self) -> Token:
+        if self.token.token_type in ("INT", "FLT"):
+            factor_token: Token = self.token
+            self.move_ahead()
+
+            return factor_token
+
+    def term(self) -> List[Token]:
+        left_node: Token | List[Token] = self.factor()
+
+        while self.token.value in ("*", "/"):
+            operator: Token = self.token
+            self.move_ahead()
+
+            right_node: Token = self.factor()
+            left_node = [left_node, operator, right_node]
+
+        return left_node
+
+    def expression(self) -> List[Token]:
+        left_node: Token | List[Token] = self.term()
+
+        while self.token.value in ("+", "-"):
+            operator: Token = self.token
+            self.move_ahead()
+
+            right_node: Token = self.term()
+            left_node = [left_node, operator, right_node]
+
+        return left_node
+
+    def parse(self) -> Token | List[Token]:
+        return self.expression()
+
+    def move_ahead(self) -> None:
+        self.idx += 1
+
+        if self.idx < len(self.tokens):
+            self.token = self.tokens[self.idx]
--- src/vadi/repl.py
+++ src/vadi/repl.py
@@ -1,3 +1,5 @@
+from parser import Parser  # type: ignore  # noqa: PGH003 # To disable pylint and ruff warnings
+
 from lexer import Lexer
 from rich.traceback import install
 from utils.console import console
@@ -9,5 +11,8 @@ while True:
     text = input()
     tokenizer: Lexer = Lexer(text)
     tokens = tokenizer.tokenize()
+    parser = Parser(tokens)
+    tree = parser.parse()
+
     if tokens is not None:
-        console.print(tokens)
+        console.print(tree)
