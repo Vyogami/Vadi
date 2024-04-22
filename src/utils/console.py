28b4131ed96340d9c25e311cd2039f3644ce7699
Shivam Shandilya <shiv.05cs@gmail.com>
2024-04-22 20:42:06 +0530

    refactor: oranise prompting and repl

--- src/utils/console.py
+++ src/utils/console.py
@@ -1,5 +1,16 @@
+import readline  # noqa: F401
+
 from rich.console import Console, Theme
 
 console_theme = Theme({"info": "blue", "warning": "yellow", "error": "bold red"})
 
 console = Console(theme=console_theme)
+
+
+def prompt():
+    """
+    Note: this is dirty fix for issue: https://github.com/Textualize/rich/issues/2293 in rich.console.input -> readline
+    """
+
+    console.print("[REPL] Vadi:", style="info", end="")
+    return input("\u00A0")
--- src/vadi/repl.py
+++ src/vadi/repl.py
@@ -1,35 +1,43 @@
-from parser import Parser  # type: ignore # To disable pylance warnings
-from typing import List
+import sys
 
-from interpreter import Interpreter
-from lexer import Lexer
-from memory import Memory
 from rich.traceback import install
-from tokens import Token
-from utils.console import console
+from utils.console import console, prompt
+
+from .interpreter import Interpreter
+from .lexer import Lexer
+from .memory import Memory
+from .parser import Parser  # type: ignore # To disable pylance warnings
 
 install()
-memory = Memory()
 
-while True:
-    console.print("[REPL] Vadi: ", style="info", end="")
-    text = input()
 
-    # if the incoming sting text is empyt then skip the iteration, empty string means <enter> | ""
-    if text == "":
-        continue
+def main() -> None:
+    memory = Memory()
 
-    try:
-        tokenizer: Lexer = Lexer(text)
-        tokens: List[Token] = tokenizer.tokenize()
+    while True:
+        try:
+            text = prompt()
+            # if the incoming sting text is empyt then skip the iteration, empty string means <enter> | ""
+            if text == "":
+                continue
 
-        parser = Parser(tokens)
-        tree = parser.parse()
+            tokenizer = Lexer(text)
+            tokens = tokenizer.tokenize()
 
-        interpreter = Interpreter(tree, memory)
-        result: Token = interpreter.interpret()
+            parser = Parser(tokens)
+            tree = parser.parse()
 
-        if result is not None:
-            console.print(result)
-    except KeyError:
-        console.print("[b][ERROR][/b]: Syntax error", style="error")
+            interpreter = Interpreter(tree, memory)
+            result = interpreter.interpret()
+
+            if result is not None:
+                console.print(result)
+        except KeyError:
+            console.print("[b][ERROR][/b]: Syntax error", style="error")
+
+        except (KeyboardInterrupt, EOFError):
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
